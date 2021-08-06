bd681811af79db9448148029fe35bd6cd504a66b
RimshaSaleem <rimshatif8@gmail.com>
2021-08-06 04:57:32 -0700

    met mvp

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import {useForm} from '../hooks/useForm';
+
 
 
 const initialValue = {
@@ -16,11 +18,11 @@ const initialValue = {
 
 const CheckoutForm = (props) => {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [values, handleChanges] = useForm(initialValue);
 
-   const handleChanges = (e) => {
-     setValues({ ...values, [e.target.name]: e.target.value });
-   };
+  //  const handleChanges = (e) => {
+  //    setValues({ ...values, [e.target.name]: e.target.value });
+  //  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
