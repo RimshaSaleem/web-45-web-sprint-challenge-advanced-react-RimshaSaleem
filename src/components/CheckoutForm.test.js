5f2248134aefc635d71798a87e30f8d368a0fa38
RimshaSaleem <rimshatif8@gmail.com>
2021-08-06 03:41:28 -0700

    done all test met mvp

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -5,7 +5,7 @@ import userEvent from "@testing-library/user-event";
 
 
 // Write up the two tests here and make sure they are testing what the title shows
-
+//  1st test for Checkout Form
 test("form header renders", () => {
 
     render(<CheckoutForm />);
